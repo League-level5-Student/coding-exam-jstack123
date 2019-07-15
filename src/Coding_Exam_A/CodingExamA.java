3d354dc9704280e89e7973c02b806c610ceefd7a
League Student <league@adminisorsiMac3.attlocal.net>
2019-07-14 17:29:06 -0700

    finish coding exam A almost done w coding exam B

--- src/Coding_Exam_A/CodingExamA.java
+++ src/Coding_Exam_A/CodingExamA.java
@@ -21,6 +21,37 @@ public class CodingExamA {
 		 * 
 		 * See the Coding_Exam_A_Demo.jar for an example of what the finished product should look like.
 		 */
-
+		String numR = JOptionPane.showInputDialog("How many robots do you want?");
+		int numRobots = Integer.parseInt(numR);
+		String color = JOptionPane.showInputDialog("Color of the shapes? (Red, Green, or Blue)");
+		String numS = JOptionPane.showInputDialog("Number of sides? (>0)");
+		int numSides = Integer.parseInt(numS);
+		
+		Thread[] robots = new Thread[numRobots];
+		
+		for (int i = 0; i < robots.length; i++) {
+			int y = i;
+			robots[i] = new Thread(() -> {
+				
+				Robot robot = new Robot();
+				robot.setSpeed(10);
+				robot.moveTo(50 + (150*y),300);
+				robot.penDown();
+				if(color.equalsIgnoreCase("red")) {
+					robot.setPenColor(Color.RED);
+				} else if(color.equalsIgnoreCase("green")) {
+					robot.setPenColor(Color.GREEN);
+				} else {
+					robot.setPenColor(Color.BLUE);
+				}
+				for (int j = 0; j < numSides; j++) {
+					robot.move(100);
+					int degree = 360/numSides;
+					robot.turn(degree);
+				}
+			});
+			robots[i].start();
+		}
+		
 	}
 }
--- src/Coding_Exam_B/CodingExamB.java
+++ src/Coding_Exam_B/CodingExamB.java
@@ -8,35 +8,79 @@ import java.io.IOException;
 
 public class CodingExamB {
 	/*
-	 * This is a logging program for collecting TODO comments in a program. The program will scan
-	 * Through all the files in the Coding_Exam_B/classes package, and collect all the comments that start
-	 * with //TODO: and write them to their own file. See the TODO_Log_example.txt file for an idea of what 
-	 * the final file output will look like.
+	 * This is a logging program for collecting TODO comments in a program. The
+	 * program will scan Through all the files in the Coding_Exam_B/classes package,
+	 * and collect all the comments that start with //TODO: and write them to their
+	 * own file. See the TODO_Log_example.txt file for an idea of what the final
+	 * file output will look like.
 	 */
-	
-	
+
 	public static String getLoggingInfo(String fileName) {
 		/*
-		 * 1. Complete the getLoggingInfoMethod.
-		 *    The method takes in a String for a file and returns a String. 
-		 *    The method will open the file a read through it. It will then 
-		 *    take all the comments that begin with //TODO: and combine them 
-		 *    into one large String. The string will also state the file name and
-		 *    the line number for where each TODO was found. 
-		*/
+		 * 1. Complete the getLoggingInfoMethod. The method takes in a String for a file
+		 * and returns a String. The method will open the file a read through it. It
+		 * will then take all the comments that begin with //TODO: and combine them into
+		 * one large String. The string will also state the file name and the line
+		 * number for where each TODO was found.
+		 */
+		int i = 0;
+		String fileLine = "File: " + fileName;
+		String line = "";
+		int lineNum = 1;
+		try {
 		
-		return "";
+			for (int j = 1; j < 50; j++) {
+				BufferedReader br = new BufferedReader(new FileReader(fileName),j);
+				
+//				while(i != -1) {
+					i = br.read();
+					String readline = br.readLine();
+					
+					if(readline.contains("//TODO")) {
+						line = lineNum + ": " + readline;
+					}
+					lineNum++;
+					if(br.read()==-1) {
+						//check if bufferedreader is at null here
+					}
+				//}
+				
+				br.close();
+			}
+		
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return fileLine + "\n" + line;
 	}
-	
+
 	public static void main(String[] args) {
 		String finalLogString = getLoggingInfo("src/Coding_Exam_B/classes/Camera.java");
 		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracedImageViewer.java");
 		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracer.java");
 		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/Vector3.java");
-		
+
 		/*
-		 * 2. Write the finalLogString to a file called TODO_Log.txt. The file should match TODO_Log_example.txt. 
+		 * 2. Write the finalLogString to a file called TODO_Log.txt. The file should
+		 * match TODO_Log_example.txt.
 		 */
+		FileWriter fw;
+		try {
+			fw = new FileWriter("TODO_Log.txt");
+			
+			fw.write(finalLogString);
+			
+			fw.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 }
